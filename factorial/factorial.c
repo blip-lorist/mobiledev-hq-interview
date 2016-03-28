60c5f2a4c0714892cf6b9f07eb5dda6ac8afafe3
Loraine <loraine@lorainekv.com>
2016-03-27 22:54:29 -0700

    Learned a ton about C and about large number factorials.

--- factorial/factorial.c
+++ factorial/factorial.c
@@ -1,43 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long long int factorial(int x);
+void factorial(int x);
+int multiply(int j, int product[], int index);
 
 /* test factorial function */
 main()
 {
-    /* Valid inputs */
-    printf("The factorial of 6 is %lld", factorial(6));
-    printf("The factorial of 19 is %lld", factorial(19));
+     factorial(0);
+     printf("is the factorial of 0.\n\n");
 
+     factorial(-1);
+     printf("is the factorial of -1.\n\n");
+     
+     factorial(32768);
+     printf("is the factorial of 32768.\n\n");
 
-    /* Invalid inputs 
-    printf("The factorial of -1 is %d", factorial(-1)); */
+     factorial(6);
+     printf("\nis the factorial of 6.\n\n");
+
+     factorial(19);
+     printf("\nis the factorial of 19.\n\n");
+
+     factorial(100);
+     printf("\nis the factorial of 100.\n\n");
+
+    factorial(8);
 }
 
 /* define the factorial function */
-long long int factorial(int x)
+void factorial(int x)
 {
-    int product;
-
     if( x <= 0 ) {
-        fprintf(stderr, "%d was provided. Please input a positive integer.\n", x);
-        exit(-1);
+        printf("%d was provided. Please input a positive integer.\n", x);
     }
     else if(x > 32767) {
-        fprintf(stderr, "This exceeds the signed integer limit. Please try a smaller input. \n");
-        exit(-1);
+        printf("This exceeds the signed integer limit. Please try a smaller input.\n");
     }
     else if( x == 1 ) {
-        product = 0;
-        return product;
+        printf("1");
     }
     else {
-        product = x;
-        while( x > 1 ) {
-            x--;
-            product = product * x;
+        /* I had no clue how to handle factorials for larger numbers, but was determined to figure it out. 
+        Mahalo to the folks who wrote these helpful articles and led me to this solution:
+        http://www.geeksforgeeks.org/factorial-large-number/
+        http://codingstreet.com/calculating-factorial-of-large-numbers-in-c/
+        */ 
+
+        /* Rather than storing our output in a single int (which has size limits), we're using an array to hold it */ 
+        int product[500], index, carry, i, j;
+        product[0] = 1;
+        index = 0;
+        
+        for(j=x; j>=2; j--) {
+            index = multiply(j, product, index);
         }
-        return product;
+
+        for(i=index; i>=0; i--) {
+            /* print our output on the fly, since no built-in data type will support numbers this large */
+            printf("%d", product[i]);
+        }
     }
+}
+
+int multiply(int j, int product[], int index) {
+        int carry = 0; 
+        int i; 
+        for (i = 0; i <= index; i++) {
+            
+            carry = (product[i]*j)+carry; 
+            product[i] = carry % 10;
+
+            carry = carry/10;
+        }
+        while (carry > 0){
+            product[++index] = carry%10;
+            carry = carry/10;
+        }
+
+        return index;
 }
